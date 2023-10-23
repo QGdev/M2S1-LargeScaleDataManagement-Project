95ad162f28ed5f71fde7b72b373d394f3d0386f1
Quentin Gomes Dos Reis <59913369+QGdev@users.noreply.github.com>
2023-10-23 10:59:03 +0200

    Update spark.sh
    Add iteration setting

--- scripts/spark.sh
+++ scripts/spark.sh
@@ -8,9 +8,10 @@
 #           - Number of worker (2)
 #           - Size of the worker disk (50)
 #           - Size of the master disk (50)
+#           - Number of iterations (3)
 #           - Project ID
 #   
-#   Example: sh run.sh gs://ermite-le-bucket/ gs://public_lddm_data/small_page_links.nt 2 50 50 largescaledataproject europe-west1 europe-west1-c
+#   Example: sh run.sh gs://ermite-le-bucket/ gs://public_lddm_data/small_page_links.nt 2 50 50 3 largescaledataproject europe-west1 europe-west1-c
 #
 
 # about
@@ -25,7 +26,6 @@ StartedDate=$(date +"%H-%M-%S")
 
 # bucket
 BucketPath=$1
-BucketPathOut=${BucketPath}out
 BucketData=$2
 
 # settings
@@ -33,19 +33,25 @@ WorkersNumber=$3
 WorkersDiskSize=$4
 MasterDiskSize=$5
 
-ProjectName=$6
+ProjectName=$7
 
 # cluster
 ClusterName=ermite-le-cluster
-Region=$7
-Zone=$8
+Region=$8
+Zone=$9
 
 Spark=pyspark
 PySpark=pagerank-notype.py
+NbIterations=$6
 
+# files names
+TimeGlobalFile=duration_results_global.txt
+TimeFile=duration_results.txt
+DataOutFile=data_out.txt
 
 # results
-DirectoryResultName=SPARK_${WorkersNumber}_${WorkersDiskSize}_${MasterDiskSize}_${Region}_${Zone}_${StartedDate}
+DirectoryResultName=PYSPARK_${WorkersNumber}_${WorkersDiskSize}_${MasterDiskSize}_${Region}_${Zone}_${StartedDate}
+BucketPathOut=${BucketPath}${DirectoryResultName}
 
 ## create the cluster
 gcloud dataproc clusters create ${ClusterName} --enable-component-gateway --region ${Region} --zone ${Zone} --master-machine-type n1-standard-4 --master-boot-disk-size ${MasterDiskSize} --num-workers ${WorkersNumber} --worker-machine-type n1-standard-4 --worker-boot-disk-size ${WorkersDiskSize} --image-version 2.0-debian10 --project ${ProjectName}
@@ -58,7 +64,7 @@ gsutil rm -rf ${BucketPathOut}
 
 ## run
 StartRuntime=$(date +%s%N)
-gcloud dataproc jobs submit ${Spark} --region ${Region} --cluster ${ClusterName} ${BucketName}pagerank-notype.py  -- ${BucketData} 3
+gcloud dataproc jobs submit ${Spark} --region ${Region} --cluster ${ClusterName} ${BucketName}pagerank-notype.py  -- ${BucketData} ${NbIterations} ${BucketPathOut} ${DataOutFile} ${TimeFile}
 EndRuntime=$(date +%s%N)
 
 echo "END OF PROCESSING PART"
@@ -67,19 +73,21 @@ echo "END OF PROCESSING PART"
 mkdir ${DirectoryResultName}
 
 # copy the result from bucket to local disk
-gsutil cp -r ${BucketPathOut} ${DirectoryResultName}
+echo ${BucketPathOut}
+echo ${DirectoryResultName}
+gsutil cp -r ${BucketPathOut}/* ${DirectoryResultName}
 
 cd ${DirectoryResultName}
 
 # Generate operation duration file
-[ -f duration_results.txt ] && rm duration_results.txt
-touch duration_results.txt
-echo "START_TIME" >> duration_results.txt
-echo ${StartRuntime} >> duration_results.txt
-echo "END_TIME" >> duration_results.txt
-echo ${EndRuntime} >> duration_results.txt
-echo "TIME" >> duration_results.txt
-echo `expr $EndRuntime - $StartRuntime` >> duration_results.txt
+[ -f ${TimeGlobalFile} ] && rm ${TimeGlobalFile}
+touch ${TimeGlobalFile}
+echo "START_TIME" >> "${TimeGlobalFile}"
+echo ${StartRuntime} >> "${TimeGlobalFile}"
+echo "END_TIME" >> "${TimeGlobalFile}"
+echo ${EndRuntime} >> "${TimeGlobalFile}"
+echo "TIME" >> "${TimeGlobalFile}"
+echo `expr $EndRuntime - $StartRuntime` >> "${TimeGlobalFile}"
 
 cd ..
 
